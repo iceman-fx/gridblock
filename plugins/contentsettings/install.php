56f5c03a3a9970d93057dd80b28d5815a1669fef
Daniel Steffen <57093581+novinet-dsteffen@users.noreply.github.com>
2021-11-16 12:36:52 +0100

    Update install.php

--- plugins/contentsettings/install.php
+++ plugins/contentsettings/install.php
@@ -3,9 +3,10 @@ if (!$this->hasConfig()) {
     // copy data directory
     rex_dir::copy($this->getPath('data'), $this->getDataPath());
     $this->setConfig('date_install', time());
-
-    $oAddon = rex_addon::get('gridblock');
-    $aConfig = $oAddon->getConfig('config');
-    $aConfig['showcontentsettingsbe'] = 'checked';
-    $oAddon->setConfig('config',$aConfig);
 }
+
+
+$oAddon = rex_addon::get('gridblock');
+$aConfig = $oAddon->getConfig('config');
+$aConfig['showcontentsettingsbe'] = 'checked';
+$oAddon->setConfig('config',$aConfig);

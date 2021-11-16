945bf9d84377f4790734e7fcbc6262a58f722be9
Daniel Steffen <57093581+novinet-dsteffen@users.noreply.github.com>
2021-11-16 12:25:11 +0100

    automatically activate contentsettings overview when install plugin

--- plugins/contentsettings/install.php
+++ plugins/contentsettings/install.php
@@ -3,4 +3,7 @@ if (!$this->hasConfig()) {
     // copy data directory
     rex_dir::copy($this->getPath('data'), $this->getDataPath());
     $this->setConfig('date_install', time());
+
+    $oAddon = rex_addon::get('gridblock');
+    $oAddon->setConfig('showcontentsettingsbe','checked');
 }

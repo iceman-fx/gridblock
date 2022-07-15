558054e422893e0f91a2ad73a5bdbb3ce14255f0
Falko Müller <fdfm2009@gmail.com>
2022-07-15 08:34:32 +0200

    Add files via upload

--- update.php
+++ update.php
@@ -15,12 +15,4 @@ $error = "";
 rex_sql_table::get(rex::getTable('1620_gridtemplates'))
 	->ensureColumn(new rex_sql_column('status', 'varchar(10)'))
 	->alter();
-
-// save gridblock module ID to config
-$db = rex_sql::factory();
-$db->setQuery('SELECT id FROM ' . rex::getTable('module') . ' WHERE input LIKE "%/* GRID_MODULE_IDENTIFIER | DONT REMOVE */%"');
-if ($db->getRows()) {
-	$aConfig = $this->getConfig('config');
-	$aConfig['gridblockmoduleid'] = $db->getValue("id");
-	$this->setConfig('config', $aConfig);
-}
+?>
